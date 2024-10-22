0b43d8d6f9d435f04131e315cadaf4a197df4f6f
Casimir Lundberg <clundber@c1r4p10.hive.fi>
2024-10-22 14:47:40 +0300

    added functions to hpp

--- module5/ex00/Bureaucrat.hpp
+++ module5/ex00/Bureaucrat.hpp
@@ -6,7 +6,28 @@
 /*   By: clundber < clundber@student.hive.fi>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 14:23:15 by clundber          #+#    #+#             */
-/*   Updated: 2024/10/22 14:23:15 by clundber         ###   ########.fr       */
+/*   Updated: 2024/10/22 14:45:57 by clundber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# pragma once
+# include <string>
+
+class Bureaucrat
+{
+private:
+	const std::string name;	
+	unsigned int grade;
+	
+public:
+	Bureaucrat();
+	Bureaucrat(Bureaucrat&);
+	~Bureaucrat();
+	
+	Bureaucrat& operator=(Bureaucrat&);
+	
+	const std::string getName();
+	unsigned int getGrade();
+};
+
+std::ostream& operator<<(std::ostream& stream, const Bureaucrat& bureaucrat);
