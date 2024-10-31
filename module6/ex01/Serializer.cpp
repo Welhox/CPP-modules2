ad4c2829138124f753643cc5712840834b1b6d30
Casimir Lundberg <clundber@c2r3p6.hive.fi>
2024-10-31 17:24:38 +0200

    ex01 done

--- module6/ex01/Serializer.cpp
+++ module6/ex01/Serializer.cpp
@@ -6,11 +6,20 @@
 /*   By: clundber < clundber@student.hive.fi>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 16:06:23 by clundber          #+#    #+#             */
-/*   Updated: 2024/10/31 16:24:46 by clundber         ###   ########.fr       */
+/*   Updated: 2024/10/31 17:23:34 by clundber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "Serializer.hpp"
+
+Serializer::Serializer(){};
+Serializer::Serializer(Serializer&){};
+Serializer::~Serializer(){};
+Serializer& Serializer::operator=(Serializer& other){
+	if (this != &other)
+		;
+	return(*this);
+};
 
 uintptr_t Serializer::serialize(Data* ptr)
 {
