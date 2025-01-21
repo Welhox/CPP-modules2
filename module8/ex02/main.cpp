a106b2edd386d41bb002b22b5c44422c5091df17
Casimir Lundberg <casimir.lundberg@gmail.com>
2025-01-21 19:59:51 +0200

    added more to ex02 main

--- module8/ex02/main.cpp
+++ module8/ex02/main.cpp
@@ -6,44 +6,56 @@
 /*   By: casimirri <clundber@student.hive.fi>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 12:20:43 by clundber          #+#    #+#             */
-/*   Updated: 2025/01/21 16:09:06 by casimirri        ###   ########.fr       */
+/*   Updated: 2025/01/21 19:59:17 by casimirri        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "MutantStack.hpp"
-
+#include<vector>
 int main()
 {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	// std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	// std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	MutantStack<int> mstack2(mstack);
-	// std::cout << mstack.top() << std::endl;
-	// std::cout << mstack2.top() << std::endl;
-	mstack.push(5);
-	mstack2 = mstack;
-	mstack.push(6);
-	// std::cout << mstack.top() << std::endl;
-	// std::cout << mstack2.top() << std::endl;	
-		
-	// [...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
+	try
 	{
-	std::cout << *it << std::endl;
-	++it;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		// std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		// std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		MutantStack<int> mstack2(mstack);
+		// std::cout << mstack.top() << std::endl;
+		// std::cout << mstack2.top() << std::endl;
+		mstack.push(5);
+		mstack2 = mstack;
+		mstack.push(6);
+		// std::cout << mstack.top() << std::endl;
+		// std::cout << mstack2.top() << std::endl;	
+
+		MutantStack<int, std::vector<int>> mstack3;
+		mstack3.push(123);
+		mstack3.push(234);
+		for (auto it : mstack3)
+			std::cout << it << std::endl;
+		
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
 	}
-	std::stack<int> s(mstack);
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	return 0;
 }
